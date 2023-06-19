870c418d05dd1b563cc445e4b6297d937720edc3
Leorio21 <Ptitlue@gmail.com>
2023-06-19 17:16:01 +0200

    Feat/refactor:
    Ajout de la valeur du range
    Modification du nom de variables

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 let isActivePerspective = true;
 
 const isPerspectiveActive = () => {
-  const element = document.getElementById("container")
-  isActivePerspective = document.getElementById("perspectChk").checked
+  const container = document.getElementById("container");
+  isActivePerspective = document.getElementById("perspectChk").checked;
   if (isActivePerspective) {
     perspective();
   } else {
-    element.style = "";
+    container.style = "";
   }
 }
 
 const perspective = () => {
+  const perspectiveValueElement = document.getElementById("perspectiveValue");
+  const perspectiveValue = document.getElementById("perspect").value;
+  perspectiveValue > 1000 && (perspectiveValue = 1000);
+  perspectiveValue < 180 && (perspectiveValue = 180);
+  perspectiveValueElement.textContent = perspectiveValue;
   if (isActivePerspective) {
-    const element = document.getElementById("container")
-    const perspective = document.getElementById("perspect").value
-    perspective > 1000 && (perspective = 1000)
-    perspective < 180 && (perspective = 180)
-    element.style = `perspective: ${perspective}px;`
+    const container = document.getElementById("container");
+    container.style = `perspective: ${perspectiveValue}px;`;
   }
 }
 
 const rotate = () => {
-  const element = document.getElementById("cube")
-  const degreeX = document.getElementById("rotateX").value
-  const degreeY = document.getElementById("rotateY").value
-  const degreeZ = document.getElementById("rotateZ").value
-  element.style = `transform: rotateX(${degreeX}deg) rotateY(${degreeY}deg) rotateZ(${degreeZ}deg);`
+  const cube = document.getElementById("cube");
+  const xValueElement = document.getElementById("rotateXValue");
+  const yValueElement = document.getElementById("rotateYValue");
+  const zValueElement = document.getElementById("rotateZValue");
+  const degreeX = document.getElementById("rotateX").value;
+  const degreeY = document.getElementById("rotateY").value;
+  const degreeZ = document.getElementById("rotateZ").value;
+  xValueElement.textContent = degreeX;
+  yValueElement.textContent = degreeY;
+  zValueElement.textContent = degreeZ;
+  cube.style = `transform: rotateX(${degreeX}deg) rotateY(${degreeY}deg) rotateZ(${degreeZ}deg);`;
 }
