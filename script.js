f5e38f770ceb2a15ca5bc7ae46ca1291bea0fa8e
Leorio21 <Ptitlue@gmail.com>
2023-06-20 11:47:46 +0200

    Feat: Ajout de picker pour changer la couleur des faces

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-let isActivePerspective = true;
+let isPerspectiveActive = true;
 
-const isPerspectiveActive = () => {
+const perspectiveCheck = () => {
   const container = document.getElementById("container");
-  isActivePerspective = document.getElementById("perspectChk").checked;
-  if (isActivePerspective) {
-    perspective();
+  isPerspectiveActive = document.getElementById("perspectChk").checked;
+  if (isPerspectiveActive) {
+    changePerspective();
   } else {
     container.style = "";
   }
 }
 
-const perspective = () => {
+const changePerspective = () => {
   const perspectiveValueElement = document.getElementById("perspectiveValue");
   const perspectiveValue = document.getElementById("perspect").value;
   perspectiveValue > 1000 && (perspectiveValue = 1000);
   perspectiveValue < 180 && (perspectiveValue = 180);
   perspectiveValueElement.textContent = perspectiveValue;
-  if (isActivePerspective) {
+  if (isPerspectiveActive) {
     const container = document.getElementById("container");
     container.style = `perspective: ${perspectiveValue}px;`;
   }
 }
 
-const rotate = () => {
+const changeRotation = () => {
   const cube = document.getElementById("cube");
   const xValueElement = document.getElementById("rotateXValue");
   const yValueElement = document.getElementById("rotateYValue");
@@ -34,4 +34,35 @@ const rotate = () => {
   yValueElement.textContent = degreeY;
   zValueElement.textContent = degreeZ;
   cube.style = `transform: rotateX(${degreeX}deg) rotateY(${degreeY}deg) rotateZ(${degreeZ}deg);`;
+}
+
+const changeColor = (el) => {
+  const face = document.getElementById(el.id);
+  let faceName = (el.id).split("");
+  faceName[0] = faceName[0].toUpperCase();
+  const newColor = document.getElementById("colorPicker".concat(faceName.join(""))).value;
+  const [colorR, colorV, colorB] = convertColor(newColor);
+  face.style = `border: 5px solid ${newColor}; background-color: rgba(${colorR}, ${colorV}, ${colorB}, .6)`;
+}
+
+const changeColorAlpha = () => {
+  const alpha = document.getElementById("colorAlpha").value;
+  const alphaValue = document.getElementById("colorAlphaValue");
+  alphaValue.textContent = alpha;
+  const picker = selectAllInputColor();
+  picker.forEach((el) => console.log(el));
+}
+
+const selectAllInputColor = () => {
+  return document.querySelectorAll("input[type=color]");
+}
+
+const convertColor = (colorToConvert) => {
+  const [_, rHex, vHex, bHex] = colorToConvert.match(/#(.{2})(.{2})(.{2})/g);
+  console.log(first)
+  const colorR = parseInt(rHex, 16);
+  const colorV = parseInt(vHex, 16);
+  const colorB = parseInt(bHex, 16);
+
+  return [colorR, colorV, colorB];
 }
